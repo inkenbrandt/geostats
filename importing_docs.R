182ce995449287c0d23371058e8bc6253771e03d
Paul <paulinkenbrandt@utah.gov>
2014-09-07 08:50:37 -0600

    added importing and ex file

---
+++ importing_docs.R
@@ -0,0 +1,15 @@
+# http://www.r-tutor.com/r-introduction/data-frame/data-import
+
+
+# to see and import sample data
+sample()
+
+beaver1
+
+# to import and assign a text file
+
+text.file <- read.table("file.txt", sep="\c", header=TRUE) 
+
+# to import and assign a text file
+
+csv.file <- read.csv("file.txt")
