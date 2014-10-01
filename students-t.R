054bc3cc2c19523866391216c9a12f1724d4dc17
inkenbrandt <paulinkenbrandt@utah.gov>
2014-10-01 07:44:30 -0600

    Updated Script

--- students-t.R
+++ students-t.R
@@ -1,20 +1,20 @@
-# from http://www.statmethods.net/advgraphs/probability.html
-# Display the Student's t distributions with various
-# degrees of freedom and compare to the normal distribution
+# Student's t distribution showing degrees of freedom vs. Normal distribution
 
-x <- seq(-4, 4, length=100)
-hx <- dnorm(x)
+# generate a series of numbers
+x <- seq(-5, 5, l=1000)
 
-degf <- c(1, 3, 8, 30)
-colors <- c("red", "blue", "darkgreen", "gold", "black")
-labels <- c("df=1", "df=3", "df=8", "df=30", "normal")
+# generate normally distributed data based on the series
+fx <- dnorm(x)
 
-plot(x, hx, type="l", lty=2, xlab="x value",
-     ylab="Density", main="Comparison of t Distributions")
+# plot the normal distribution
+plot(x, fx, type="l", lty=2, xlab="x", lwd = 2,
+     ylab="P(x)", main="Student's t Distribution")
 
-for (i in 1:4){
-  lines(x, dt(x,degf[i]), lwd=2, col=colors[i])
-}
+# plot the student's t distributions
+lines(x, dt(x, 1), col = 4)
+lines(x, dt(x, 2), col = 3)
+lines(x, dt(x, 20), col = 6)
 
-legend("topright", inset=.05, title="Distributions",
-       labels, lwd=2, lty=c(1, 1, 1, 1, 2), col=colors)
+# add a legend
+legend("topleft", lwd = c(1, 1, 1, 2), bty = "n", c("df=1", "df=2", "df=20", "Normal"), 
+       lty = c(1, 1, 1, 2), col = c(4, 3, 6, 1))
