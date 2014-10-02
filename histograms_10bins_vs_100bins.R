c39081030bdb97c9740c17b1d611a11e80eb6441
inkenbrandt <paulinkenbrandt@utah.gov>
2014-10-02 07:48:24 -0600

    added comments

--- histograms_10bins_vs_100bins.R
+++ histograms_10bins_vs_100bins.R
@@ -1,9 +1,13 @@
+
+# generate 10,000 normally distributed datapoints
 x <- rnorm(10000,0,1)
 
 #allow for stacking of figures 1 row and 2 columns in size
 par(mfrow=c(1,2))
 
-
+# create a histogram using the x data and 10 bins
 hist(x, breaks = seq(min(x), max(x), l = 11), col = 2, xlim = c(-5,5), ylim = c(0,3500), main="10 Bins")
+
+# create a histogram using the x data and 10 bins
 hist(x, breaks = seq(min(x), max(x), l = 101), col = 2, xlim = c(-5,5), ylim = c(0,400), main="100 Bins")
 
