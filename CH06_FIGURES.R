66ff8d73427b393303a90c56b3d65676a8a6a54e
Paul <paulinkenbrandt@utah.gov>
2014-10-13 11:31:37 -0600

    ch6

---
+++ CH06_FIGURES.R
@@ -0,0 +1,15 @@
+fileplace <- 'E:/STATS/Review/original figures/'
+####################################################################################
+# Figure 06-
+
+# http://pascencio.cos.ucf.edu/classes/Methods/R%20demos/R%20Demonstration%20Law%20Large%20numbers%202012.pdf
+
+sample <- rnorm(1,3.5,1.709)
+
+#sample <- rbinom(1,30, 1/6) 
+
+
+for (i in 1:1000){
+  sample<-rnorm([i], mean=mu, sd=sigma),
+  mean_vector<- mean(sample[i])
+  }
