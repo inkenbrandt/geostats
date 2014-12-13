bb2eda0e7840c56249bf4cfec07cd02a6f47ee8f
Paul <paulinkenbrandt@utah.gov>
2014-12-12 22:28:43 -0700

    updates

--- CH06_FIGURES.R
+++ CH06_FIGURES.R
@@ -1,10 +1,14 @@
-fileplace <- 'E:/STATS/Review/original figures/'
+fileplace <- 'E:/STATS/Review/Chapters/original figures/'
 ####################################################################################
 # Figure 06-
 
 # http://pascencio.cos.ucf.edu/classes/Methods/R%20demos/R%20Demonstration%20Law%20Large%20numbers%202012.pdf
 
 #sample <- rbinom(1,30, 1/6) 
+
+# create figures for file
+filename <- 'Figure 06-04'
+pdf(paste(fileplace,filename,'.pdf'))
 
 plot(1,mean(rnorm(i,3.5,1.709)),xlim=c(0,1000), main = 'Law of Large Numbers', 
      ylab = 'Mean Result', xlab = 'Number of Dice Rolls')
@@ -15,3 +19,4 @@ for (i in 1:1000) points(i,mean(rnorm(i,3.5,1.709)))
 
 
 lines(c(-100,1100), c(3.5,3.5), col='red', lty = 2, lwd = 2)
+
